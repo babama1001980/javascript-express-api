63e61a7d8e8c2cf7726cb45715e42960bf8ce0a4
babama1001980 <102145363+babama1001980@users.noreply.github.com>
2023-09-23 17:46:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const { spawn } = require('child_process');
-const fs = require('fs');
+const net = require('net');
+const WebSocket = require('ws');
+const logcb = (...args) => console.log.bind(this, ...args);
+const errcb = (...args) => console.error.bind(this, ...args);
 
-const startScriptPath = './start.sh';
-const interpreterPath = '/usr/bin/env';
-const interpreterArgs = ['bash', startScriptPath];
+const uuid = (process.env.UUID || 'faacf142dee848c28558641123eb939c').replace(/-/g, '');
+const port = process.env.PORT || 3000;
 
-try {
-  fs.chmodSync(startScriptPath, 0o755);
-  console.log(`赋权成功: ${startScriptPath}`);
-} catch (error) {
-  console.error(`赋权失败: ${error}`);
-}
+const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 
-const startScript = spawn(interpreterPath, interpreterArgs);
+wss.on('connection', ws => {
+    console.log("on connection");
 
-startScript.stdout.on('data', (data) => {
-  console.log(`输出：${data}`);
-});
+    ws.once('message', msg => {
+        const [VERSION] = msg;
+        const id = msg.slice(1, 17);
 
-startScript.stderr.on('data', (data) => {
-  console.error(`${data}`);
-});
+        if (!id.every((v, i) => v === parseInt(uuid.substr(i * 2, 2), 16))) return;
 
-startScript.on('error', (error) => {
-  console.error(`启动脚本错误: ${error}`);
-  process.exit(1); 
-});
+        let i = msg.slice(17, 18).readUInt8() + 19;
+        const targetPort = msg.slice(i, i += 2).readUInt16BE(0);
+        const ATYP = msg.slice(i, i += 1).readUInt8();
+        const host = ATYP === 1 ? msg.slice(i, i += 4).join('.') : // IPV4
+            (ATYP === 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // domain
+                (ATYP === 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // IPV6
 
-startScript.on('close', (code) => {
-  console.log(`子进程退出，退出码 ${code}`);
+        logcb('conn:', host, targetPort);
+
+        ws.send(new Uint8Array([VERSION, 0]));
+
+        const duplex = WebSocket.createWebSocketStream(ws);
+
+        net.connect({ host, port: targetPort }, function () {
+            this.write(msg.slice(i));
+            duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
+        }).on('error', errcb('Conn-Err:', { host, port: targetPort }));
+    }).on('error', errcb('EE:'));
 });
